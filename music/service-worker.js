50537fc01c2ed58974092384e64e485c2e096bb8
Fred <nagual@MacBook-Pro-de-Fred.local>
2025-11-09 17:03:51 -0300

    auto

--- music/service-worker.js
+++ music/service-worker.js
@@ -1,33 +1,15 @@
-const CACHE_NAME = "droptv-music-v1";
-const URLS_TO_CACHE = [
-  "/music/",
-  "/music/index.html",
-  "/music/style.css",
-  "/music/app.js",
-  "/music/cover.jpg"
+const CACHE_NAME = 'droptv-radio-v1';
+const FILES = [
+  '/music/',
+  '/music/index.html',
+  '/music/cover.jpg',
+  '/music/manifest.json'
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
-  );
-  console.log("Service Worker instalado.");
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-    ))
-  );
-  console.log("Service Worker ativo.");
-});
-
